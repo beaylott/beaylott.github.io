a3ca7315c9c73c2f542ba7331b578e7fdf59ca2b
Ben Aylott <ben@1d2f0.info>
2025-01-05 18:56:17 +0000

    update habits

--- habits/generate_graph.py
+++ habits/generate_graph.py
@@ -7,5 +7,5 @@ dates_and_data = pd.read_csv("2025.csv", sep=",", header=None).to_numpy()
 dates = dates_and_data[:, 0]
 data = dates_and_data[:, 1]
 
-july.heatmap(dates, data, title="Habit tracker 2025", cmap="github")
+july.heatmap(dates, data, title="Habit tracker", cmap="github")
 plt.savefig("habits2025.png")
